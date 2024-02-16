00edce63ab55c50b107c590e6229ca894f035058
Valentina Bonich <vbonich@gmail.com>
2024-02-16 20:23:15 -0300

    actualizar

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Tripleten web_project_around
+
+Proyecto 5
+Este es un proyecto llamado "Alrededor de los EE.UU.", una página interactiva en la que los usuarios pueden añadir, eliminar o dar "me gusta" a las fotos.
+
+Esta página web se verá de forma correcta en diferentes tamaños y resoluciones de pantalla, ya sea en celulares, tablets o en computadores.
+
+Para el diseño se utiliza un prototipo de Figma:
+https://www.figma.com/file/mUgu8OSHWE0M6p6vfwmdu9/Sprint-4%3A-Around-The-U.S.-%2F-desktop-%2B-mobile?type=design&node-id=0-1&mode=design&t=ctvrnokwNIRTq2Pd-0
+
+Tecnologías utilizadas:
+HTML5 CSS NORMALIZE
+Prettier como formateador automático del código
+Nojekill para desplegar de manera correcta el sitio en GitHub Pages.
+Metodología BEM para organizar la estructura de la página, sus bloques, elementos y modificaciones.
+Javascript como lenguaje de programación que me permite darle interactividad y lógica a la página web.
+
+El proyecto se puede visualizar vía online en el sitio de GitHub Pages:
+https://github.com/Valentina-Bonich/web_project_around
---
+++ index.js
@@ -0,0 +1,28 @@
+const popUpProfile = document.querySelector("#popup-profile");
+const btnProfile = document.querySelector(".profile__button");
+const btnCloseProfile = document.querySelector("#close-profile-form");
+const profileName = document.querySelector(".profile__name");
+const profileParagraph = document.querySelector(".profile__paragraph");
+const inputProfileName = document.querySelector("#input-name");
+const inputProfileAbout = document.querySelector("#input-about");
+const btnSaveProfile = document.querySelector("#profile-form-button");
+const formProfile = document.querySelector("#form-profile");
+
+function handleOpenProfileForm() {
+  popUpProfile.classList.add("popup_show");
+}
+
+function handleCloseProfileForm() {
+  popUpProfile.classList.remove("popup_show");
+}
+
+function handleProfileSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputProfileName.value;
+  profileParagraph.textContent = inputProfileAbout.value;
+  handleCloseProfileForm();
+}
+
+btnProfile.addEventListener("click", handleOpenProfileForm);
+btnCloseProfile.addEventListener("click", handleCloseProfileForm);
+formProfile.addEventListener("submit", handleProfileSubmit);
