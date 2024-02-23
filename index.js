6b2bbe40e64f75a7077b87c473040c6d1890996f
Valentina Bonich <vbonich@gmail.com>
2024-02-23 18:15:58 -0300

    feat:actualizar

--- index.js
+++ index.js
@@ -7,7 +7,39 @@ const inputProfileName = document.querySelector("#input-name");
 const inputProfileAbout = document.querySelector("#input-about");
 const btnSaveProfile = document.querySelector("#profile-form-button");
 const formProfile = document.querySelector("#form-profile");
+const btnAddCard = document.querySelector(".profile__add-button");
+const popUpCard = document.querySelector("#popup-add-card");
+const btnCloseCard = document.querySelector("#close-addcard-form");
+const templateCard = document.querySelector(".template-card");
 
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+//abrir y cerrar profile form
 function handleOpenProfileForm() {
   popUpProfile.classList.add("popup_show");
 }
@@ -23,6 +55,26 @@ function handleProfileSubmit(evt) {
   handleCloseProfileForm();
 }
 
+//abrir y cerrar card form
+function handleOpenCardForm() {
+  popUpCard.classList.add("popup_show");
+}
+
+function handleCloseCardForm() {
+  popUpCard.classList.remove("popup_show");
+}
+
+function CardGenerator(title, url) {
+  const cardClone = templateCard.textContent.querySelector(".element");
+  const cardImage = cardClone.querySelector(".element__photo");
+  const cardTitle = cardClone.querySelector(".element__photo-name");
+  cardImage.src = link;
+  cardTitle.textContent = title;
+}
+
 btnProfile.addEventListener("click", handleOpenProfileForm);
 btnCloseProfile.addEventListener("click", handleCloseProfileForm);
 formProfile.addEventListener("submit", handleProfileSubmit);
+
+btnAddCard.addEventListener("click", handleOpenCardForm);
+btnCloseCard.addEventListener("click", handleCloseCardForm);
